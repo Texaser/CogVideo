28d90b8cb8327f9a1c25b165b7b1709f11c40ca4
Han Yi <hanyi@bumblebee.cs.unc.edu>
2024-09-16 23:02:54 -0400

    update

--- sat/inference.sh
+++ sat/inference.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-environs="CUDA_VISIBLE_DEVICES=3 WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
+environs="CUDA_VISIBLE_DEVICES=4,5,6,7 WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
 
-run_cmd="$environs python sample_video.py --base configs/cogvideox_2b_lora.yaml configs/inference.yaml --seed $RANDOM"
+run_cmd="$environs python sample_video.py --base configs/cogvideox_5b_lora.yaml configs/inference.yaml --seed $RANDOM"
 
 echo ${run_cmd}
 eval ${run_cmd}
