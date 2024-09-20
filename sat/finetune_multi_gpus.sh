dd9324b9c4acb01ee158db5db244f4c10cd4cda2
Han Yi <hanyi@bumblebee.cs.unc.edu>
2024-09-19 20:07:50 -0400

    update

--- sat/finetune_multi_gpus.sh
+++ sat/finetune_multi_gpus.sh
@@ -1,16 +1,4 @@
-#!/bin/bash
-
-# Set the path to your Anaconda environment
-CONDA_ENV_PATH="/mnt/mir/fan23j/anaconda3/envs/cogvideo"
-
-# Add the Anaconda environment's bin directory to the PATH
-export PATH="$CONDA_ENV_PATH/bin:$PATH"
-
-# Specify the full path to the Python interpreter
-PYTHON_PATH="$CONDA_ENV_PATH/bin/python"
-
-# Set up the run command using the specified Python interpreter
-run_cmd="CUDA_VISIBLE_DEVICES=2,3 $PYTHON_PATH -m torch.distributed.run --standalone --nproc_per_node=2 train_video.py --base configs/cogvideox_2b_lora.yaml configs/sft.yaml --seed $RANDOM"
+run_cmd="CUDA_VISIBLE_DEVICES=1,2 torchrun --standalone --nproc_per_node=2 train_video.py --base configs/cogvideox_2b_lora.yaml configs/sft.yaml --seed $RANDOM"
 
 echo ${run_cmd}
 eval ${run_cmd}
